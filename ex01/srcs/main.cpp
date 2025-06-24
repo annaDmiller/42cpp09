262ccc0bfd56f09b3c939bc7da2cbb0ab6185eb4
Anna Melnikova <amelniko@student.42.fr>
2025-06-24 20:31:44 +0200

    Minor fix for main to return correct value and print Error all the time

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -90,7 +90,10 @@ void runCalculation(char* arg)
 int main(int argc, char **argv)
 {
     if (argc != 2)
+    {
+        std::cerr << "Error" << std::endl;
         return (1);
+    }
     
     try
     {
@@ -99,6 +102,7 @@ int main(int argc, char **argv)
     catch(const std::exception& e)
     {
         std::cerr << e.what() << std::endl;
+        return (1);
     }
     
     return (0);
